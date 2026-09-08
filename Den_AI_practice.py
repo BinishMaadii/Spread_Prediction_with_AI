f340464585483ffba58050f1ee9914ceafa773c1
binish batool <31070645+BinishMaadii@users.noreply.github.com>
2026-09-08 23:16:54 +0200

    Add initial imports for AI practice script

---
+++ Den_AI_practice.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import joblib
+import shap
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+import statsmodels.api as sm 
+
+
